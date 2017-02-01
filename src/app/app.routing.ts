255df05d70bfa36fdf393ad3baa2de4a4ac7d968
Daniel Ha <dha@interweb-solutions.com>
2017-02-01 10:24:22 -0800

    Initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by griga on 7/11/16.
+ */
+
+import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from "./shared/layout/app-layouts/main-layout.component";
+import {AuthLayoutComponent} from "./shared/layout/app-layouts/auth-layout.component";
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '', redirectTo: 'sites', pathMatch: 'full'
+            },
+            {
+                path: 'sites',
+                loadChildren: 'app/+home/home.module#HomeModule'
+            },
+        ]
+    },
+
+];
+
+export const appRouting = RouterModule.forRoot(routes);
